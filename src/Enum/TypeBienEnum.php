0714324bff31f4fb31c57b5ff060e7d6d9c6a91f
nfrevol <nfrevol@com-company.fr>
2025-07-18 17:54:12 +0200

    feat: ajout du TypeBienEnum

---
+++ src/Enum/TypeBienEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ComCompany\PhpEdlContracts\Enum;
+
+enum TypeBienEnum
+{
+    case APPARTEMENT;
+    case MAISON;
+    case GARAGE;
+    case LOCAL;
+    case AUTRE;
+}
